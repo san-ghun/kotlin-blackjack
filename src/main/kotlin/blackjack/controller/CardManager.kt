2d43190ba6cdaf3867c62bf4c31cd8d93b41e425
Sanghun Park <po4865@gmail.com>
2025-07-18 13:35:40 +0200

    refactor(CardManager): remove a property 'cardMap' for non-usage

--- src/main/kotlin/blackjack/controller/CardManager.kt
+++ src/main/kotlin/blackjack/controller/CardManager.kt
@@ -4,31 +4,9 @@ import blackjack.model.PlayingCard
 
 class CardManager() {
     var cards: List<PlayingCard> = emptyList()
-    private var _cardMap: Map<PlayingCard, Boolean> = emptyMap()
-    val cardMap: Map<PlayingCard, Boolean>
-        get() = _cardMap
 
     init {
         cards = PlayingCard.Deck.toList().shuffled()
-        _cardMap = initiateCardMap()
-    }
-
-    private fun initiateCardMap(): Map<PlayingCard, Boolean> {
-        val map = mutableMapOf<PlayingCard, Boolean>()
-        cards.forEach { card ->
-            map[card] = true
-        }
-        return map
-    }
-
-    fun checkCardMap(card: PlayingCard): Boolean {
-        return cardMap.getOrDefault(card, false)
-    }
-
-    private fun updateCardMap(card: PlayingCard) {
-        val map = cardMap.toMutableMap()
-        map[card] = false
-        _cardMap = map.toMap()
     }
 
     private fun getCard(): PlayingCard {
@@ -41,7 +19,6 @@ class CardManager() {
 
     fun giveCard(): PlayingCard {
         val card = getCard()
-        updateCardMap(card)
         return card
     }
 }
